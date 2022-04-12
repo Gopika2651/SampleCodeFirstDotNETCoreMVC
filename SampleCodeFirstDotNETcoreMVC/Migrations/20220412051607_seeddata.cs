1f039310ecd2aa5183f8e65d3bf1afe1d6099d75
GOPIKA V <sindhugopika2651@gmail.com>
2022-04-12 14:24:40 +0530

    Add project files.

---
+++ SampleCodeFirstDotNETcoreMVC/Migrations/20220412051607_seeddata.cs
@@ -0,0 +1,57 @@
+﻿sing Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SampleCodeFirstDotNETcoreMVC.Migrations
+{
+    public partial class seeddata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsComplete",
+                table: "TodoItem",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 1L, "In meeting need to discuss some points.", true, "Meeting with management" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 2L, "List of this this item buy.", false, "Go for shopping" });
+
+            migrationBuilder.InsertData(
+                table: "TodoItem",
+                columns: new[] { "Id", "Description", "IsComplete", "Name" },
+                values: new object[] { 3L, "here is a task to do on a call", true, "Call for someone to do some task" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "TodoItem",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "IsComplete",
+                table: "TodoItem",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(bool));
+        }
+    }
+}
---
+++ SampleCodeFirstDotNETcoreMVC/Models/ToDoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace SampleCodeFirstDotNETcoreMVC.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItem { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+        {
+           
+             modelBuilder.Entity<TodoItem>().HasData(
+              new TodoItem() { Id = 1, Name = "Meeting with management", IsComplete = true, Description = "In meeting need to discuss some points." },
+              new TodoItem() { Id = 2, Name = "Go for shopping", IsComplete = false, Description = "List of this this item buy." },
+              new TodoItem() { Id = 3, Name = "Call for someone to do some task", IsComplete = true, Description = "here is a task to do on a call" });
+
+        }
+    }
+}
